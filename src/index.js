01b031aeb895f42ff08b936395a839b8cc2c0934
vuong.tk <vuong.tk@gnt.com.vn>
2019-02-20 11:19:20 +0700

    fix cros error

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import path from 'path';
 
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/api/auth', (req, res) => {
+    console.log('come here ?')
     res.status(404).json({ errors: { global: 'invalid credentials' } });
 })
 
 app.listen(8080, () => console.log('running in port 8080'))
+
